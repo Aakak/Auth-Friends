8f9df00d1035ce159482c7946f098f94e6bbf893
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-15 22:48:09 -0700

    Added React loader

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Friend = ({ friendInfo }) => {
   return (
     <div>
-        HI
       <h2>{friendInfo.name}</h2>
       <p>Age: {friendInfo.age}</p>
       <p>email: {friendInfo.email}</p>
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react'
 import axiosWithAuth from '../utils/axiosWithAuth.js'
 import AddNewFriend from './AddNewFriend';
 import Friend from './Friend';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from 'react-loader-spinner';
+ 
 
 const FriendsList = () => {
   const [friends, setFriends] = useState([])
+ 
 
   const update= () => {
     axiosWithAuth()
@@ -22,7 +26,15 @@ const FriendsList = () => {
   return(
     <>
       <AddNewFriend  handler={update}/>
-      <h1>Friend List!</h1>
+      <h1>Friend List</h1>
+      <Loader
+         type="Puff"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={2000} //3 secs
+
+      />
       {friends.map(friend => (
         <div key={friend.id}>
           <Friend friendInfo={friend} />
